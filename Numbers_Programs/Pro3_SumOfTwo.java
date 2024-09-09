227d579324782ac57669ce6113cea593f5ce60d8
Satish Nirmal <120482454+satish-nirmal@users.noreply.github.com>
2024-09-09 20:55:04 +0530

    Add files via upload

---
+++ Numbers_Programs/Pro3_SumOfTwo.java
@@ -0,0 +1,29 @@
+package com.java.numberprograms;
+
+public class Pro3_SumOfTwo {
+
+	public static int sum(int a, int b) {
+
+		int c = a + b;
+
+		System.out.println("c : " + c);
+
+		return c;
+
+	}
+	public static void main(String[] args) {
+
+//		using method 
+//		sum(11, 20);
+
+//		Without using method
+
+		int a = 10, b = 80;
+
+		int c = a + b;
+
+		System.out.println(c);
+
+	}
+
+}
