9c13467440978e39a3999472a6a7fc15ab9ece8c
Satish Nirmal <120482454+satish-nirmal@users.noreply.github.com>
2024-09-11 11:52:31 +0530

    Add files via upload

---
+++ Numbers_Programs/Pro8_Data_Types.java
@@ -0,0 +1,27 @@
+package com.java.numberprograms;
+
+public class Pro8_Data_Types {
+
+	static byte b;
+	static short s;
+	static int i;
+	static long l;
+	static double d;
+	static boolean bl;
+	static char c;
+	static float f;
+
+	public static void main(String[] args) {
+
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(d);
+		System.out.println(bl);
+		System.out.println(c);
+		System.out.println(f);
+
+	}
+
+}
