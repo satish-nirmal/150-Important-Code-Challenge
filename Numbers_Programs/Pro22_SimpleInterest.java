ad4d61e42ac30178c91598e14fd16b20ed8400e8
Satish Nirmal <120482454+satish-nirmal@users.noreply.github.com>
2024-09-21 19:20:53 +0530

    Add files via upload

---
+++ Numbers_Programs/Pro22_SimpleInterest.java
@@ -0,0 +1,22 @@
+package com.java.numberprograms;
+
+/**
+ * Question :
+ * 1. Java Program to Calculate Simple Interest. - (GFG)
+ */
+/**
+ * @author Satish
+ */
+
+public class Pro22_SimpleInterest {
+
+	public static void main(String[] args) {
+
+		float p = 10000, r = 5, t = 5;
+
+		float si = (p * r * t) / 100;
+
+		System.out.println("Output : " + si);	
+	}
+
+}
