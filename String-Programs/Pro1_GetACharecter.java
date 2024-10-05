a004b1c063228d65769da56f8563a09d6f6c71c0
Satish Nirmal <120482454+satish-nirmal@users.noreply.github.com>
2024-10-06 00:31:23 +0530

    Add files via upload

---
+++ String-Programs/Pro1_GetACharecter.java
@@ -0,0 +1,16 @@
+package com.java.code.string;
+
+public class Pro1_GetACharecter {
+
+	public static void main(String[] args) {
+
+		String str = "dkjeflknkdco";
+
+		int index = 5;
+		
+		char charAt = str.charAt(index);
+		
+		System.out.println(charAt);
+	}
+
+}
